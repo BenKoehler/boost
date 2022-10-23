795c3a2af923a9d65b8a98552f1682eefcf07b9b
BenKoehler <ben.koehler@web.de>
2022-10-23 11:17:34 +0200

    - disable icu for regex - add install to build script - build without version number file suffixes - use local zlib

--- bk-install.sh
+++ bk-install.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-./bootstrap.sh
-./b2 
+cd ..
+mv boost/ boost-src/
+#remove previous installation
+rm -r boost
+mkdir boost
+cd boost-src
+./bootstrap.sh --without-icu
+./b2 clean
+./b2 headers
+./b2 \
+-j8 \
+--disable-icu \
+--ignore-site-config \
+--prefix=../boost \
+--layout=system \
+variant=release \
+threading=multi \
+cxxflags="-fPIC -std=c++17 -O3" \
+link=shared \
+runtime-link=static \
+-sZLIB_INCLUDE=../zlib/include \
+-sZLIB_LIBPATH=../zlib/lib \
+install
+cd ..
+cp boost-src/bk.cmake boost
